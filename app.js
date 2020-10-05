3ff56d29d36e0967e96fb1f3f44e04d6fd148d84
Luiz Gontijo Jr <luizgontijojr@Luizs-MacBook-Pro.local>
2020-10-04 18:50:55 -0700

    Hide all secrets

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const firebase = require("firebase");
 const bodyParser = require("body-parser");
@@ -7,6 +9,17 @@ firebase.initializeApp(firebaseConfig);
 const app = express();
 const port = process.env.PORT || "8000";
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
 
 app.get("/auth/signup", (req, res) => {
   const { email, password } = req.query;
